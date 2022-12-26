3151b1e82291357fa757b15b8a96b6eafbfc52bd
Davy De Groote <davy.degr@gmail.com>
2022-12-26 14:41:44 +0100

    Updated functions, added check for all colors, first steps for delete buttons

--- README.md
+++ README.md
@@ -13,13 +13,18 @@ A tool to count scores in the Machiavelli game.
 - Make it able to change player names from 'Player 1' to something chosen
 - Automate the start of the game to ask for player names
 - DONE: Update functions so they can be used independently from the total amount of players
-- Calculate extra points if player has all colors
-- Calculate extra points if player has 7 buildings for the first time
-- Calculate extra points if player has 7 buildings after someone else has got 7 buildings
+- DONE: Calculate extra points if player has all colors
+- Calculate extra points if player has 8 buildings for the first time
+- Calculate extra points if player has 8 buildings after someone else has got 8 buildings
 - Disable buttons after one person reaches 7 buildings and the whole round is played
 
 ## Specific to do's
 
 - Determine how you can get the unique DOM identifier for the element you just created, add it to the {allBuildings} as a new property. Use it to later identify the item that a user wants to delete.
-- Add a hidden 'id' number to every building <li> item, making it easy to identify which one to delete
+- DONE: Add a hidden 'id' number to every building <li> item, making it easy to identify which one to delete
 - Make the flash message be of position absolute and animate it
+
+## Learnings
+
+- You can create a html element by using backticks. Than add that element inside or outside an already existing element by using .insertAdjacentHTML('beforeend', htmlContent);`
+- You can combine string interpolation and for loops in document.querySelectors
--- script.js
+++ script.js
@@ -5,45 +5,73 @@ let scoreDisplay;
 let flashMessageP1;
 let score = 0;
 
+// Initialize the unique ID number, incrementing everytime a building is created
+let uniqueID = 1;
+
 // FUNCTIONS
 
 // Updates the buildings list with the current array
 function updateBuildings(playerNumber) {
-  // Initialize an empty object to store the requested list of buildings
-  // let list = document.querySelector('#currentBuildings--p1');
+  // Grab the <ul> element of the requested player
   let list = document.querySelector(`#currentBuildings--p${playerNumber}`);
 
-  // Empty all the HTMl inside this list (which is an <ul>)
+  // Empty all the HTMl inside this list
   list.innerHTML = '';
 
   // Loop over [buildings] to create a list of all current buildings of the player
   for (let i = 0; i < allBuildings.length; i++) {
     if (allBuildings[i]['player'] == playerNumber) {
+      const htmlContent = `
+      <li>${allBuildings[i]['score']} - ${allBuildings[i]['color']}<span class="delete-btn"> - verwijderen</span><span class="building-id hidden">${allBuildings[i]['uniqueID']}</span></li>
+      `;
+
+      list.insertAdjacentHTML('beforeend', htmlContent);
+
       // Create new <li>
-      const newItem = document.createElement('li');
+      // const newItem = document.createElement('li');
 
-      // Create content for the <li>
-      const itemContent = document.createTextNode(
-        `${allBuildings[i]['score']} - ${allBuildings[i]['color']}`
-      );
+      // // Create content for the <li>
+      // const itemContent = document.createTextNode(
+      //   `${allBuildings[i]['score']} - ${allBuildings[i]['color']}`
+      // );
 
-      // Add the content to the item
-      newItem.appendChild(itemContent);
+      // // Add the content to the item
+      // newItem.appendChild(itemContent);
 
-      list.append(newItem);
+      // list.append(newItem);
     }
   }
+
+  calculateTotal(playerNumber);
+  addDeleteButtons();
 }
 
 // Calculate the total score of the player and update the front-end
 function calculateTotal(playerNumber) {
   let total = 0;
+  let colors = [];
 
+  // Loop over all the buildings and add the total to the score
   for (let i = 0; i < allBuildings.length; i++) {
     if (allBuildings[i]['player'] == playerNumber) {
       total += allBuildings[i]['score'];
+
+      // Add the color to the colors array
+      colors.push(allBuildings[i]['color']);
     }
   }
+
+  if (
+    colors.indexOf('red') != -1 &&
+    colors.indexOf('green') != -1 &&
+    colors.indexOf('blue') != -1 &&
+    colors.indexOf('yellow') != -1 &&
+    colors.indexOf('purple') != -1
+  ) {
+    total += 3;
+  }
+
+  scoreDisplay = document.querySelector(`#currentScore--player${playerNumber}`);
 
   scoreDisplay.textContent = total;
 }
@@ -72,6 +100,7 @@ function getCheckedPlayerNumber() {
 
 // MAIN PROJECT
 //TODO: Delete test data, just initialize as an empty array
+
 let allBuildings = [
   {
     score: 7,
@@ -82,15 +111,30 @@ let allBuildings = [
 
   {
     score: 7,
-    color: 'neutral',
+    color: 'blue',
     player: 1,
     uniqueID: 101,
   },
+
+  {
+    score: 1,
+    color: 'green',
+    player: 1,
+    uniqueID: 102,
+  },
+
   {
     score: 100,
-    color: 'red',
-    player: 2,
-    uniqueID: 102,
+    color: 'yellow',
+    player: 1,
+    uniqueID: 103,
+  },
+
+  {
+    score: 100,
+    color: 'purple',
+    player: 1,
+    uniqueID: 104,
   },
 ];
 
@@ -124,17 +168,10 @@ for (let i = 1; i <= 6; i++) {
   flashMessageP1 = document.querySelector(`#flash-p${i}`);
 
   updateBuildings(i);
-  calculateTotal(i);
+  // calculateTotal(i);
 }
 
-//TODO: Use a for loop to update buildings for all players
-
-// TODO: Initiate the game with 6 players, creating all elements through JavaScript with a for loop
-
 // Add a building and display a flash message
-
-// Initialize the unique ID number, incrementing everytime a building is created
-let uniqueID = 1;
 
 document.querySelector('#add-building').addEventListener('click', function () {
   // Get input fields
@@ -142,41 +179,72 @@ document.querySelector('#add-building').addEventListener('click', function () {
   const buildingValue = getBuildingValue();
   const playerNumber = getCheckedPlayerNumber();
 
-  // Construct object to add
-  const newBuilding = {
-    color: buildingColor,
-    score: buildingValue,
-    uniqueID: uniqueID,
-    player: playerNumber,
-  };
+  if (
+    buildingColor != undefined &&
+    buildingValue != undefined &&
+    playerNumber != undefined
+  ) {
+    // Construct object to add
+    const newBuilding = {
+      color: buildingColor,
+      score: buildingValue,
+      uniqueID: uniqueID,
+      player: playerNumber,
+    };
 
-  // Increment uniqueID to keep it unique
-  uniqueID += 1;
+    // Increment uniqueID to keep it unique
+    uniqueID += 1;
 
-  // Push object to array
-  allBuildings.push(newBuilding);
+    // Push object to array
+    allBuildings.push(newBuilding);
 
-  // Update buildings list & total points
-  updateBuildings(playerNumber);
-  calculateTotal(playerNumber);
+    // Update buildings list & total points
+    updateBuildings(playerNumber);
+    // calculateTotal(playerNumber);
 
-  // Flash a message on the screen
-  flashMessageP1.textContent = `Added a ${buildingColor} building of score ${buildingValue}`;
+    // Flash a message on the screen
+    flashMessageP1.textContent = `Buildingcolor: ${buildingColor} / Buildingvalue: ${buildingValue} / uniqueID: ${uniqueID} / player number: ${playerNumber}`;
 
-  flashMessageP1.classList.remove('hidden');
+    flashMessageP1.classList.remove('hidden');
 
-  // Hide messages after 5 seconds
-  setTimeout(() => {
-    flashMessageP1.classList.add('hidden');
-  }, '5000');
+    // Hide messages after 5 seconds
+    setTimeout(() => {
+      flashMessageP1.classList.add('hidden');
+    }, '5000');
+  }
 });
 
 // TODO: Delete a building
 // Select all delete buttons
-const deleteBtns = document.querySelectorAll('.delete-btn');
-// Loop over all buildings and add an event listener to the delete button
-for (let i = 0; i < deleteBtns.length; i++) {
-  deleteBtns[i].addEventListener('click', function () {
-    console.log('btn clicked');
-  });
+// let deleteBtns = document.querySelectorAll('.delete-btn');
+
+function addDeleteButtons() {
+  let deleteBtns = document.querySelectorAll('.delete-btn');
+
+  // Loop over all buildings and add an event listener to the delete button
+  for (let i = 0; i < deleteBtns.length; i++) {
+    // Execute when a delete button is clicked
+    deleteBtns[i].addEventListener('click', function () {
+      // get the itemID of the clicked item
+      let itemID = Number(deleteBtns[i].nextSibling.innerHTML);
+      console.log(itemID);
+      let player = 0;
+
+      // Loop over allBuildings and delete that item
+      for (let i = 0; i < allBuildings.length; i++) {
+        if (allBuildings[i]['uniqueID'] == itemID) {
+          // Grab the player id so you can call updateBuildings
+          player = allBuildings[i]['player'];
+
+          // TODO: Delete this item
+          allBuildings.splice(i, 1);
+          console.log(`Item with id ${itemID} deleted`);
+          break;
+        }
+      }
+
+      updateBuildings(player);
+      deleteBtns = document.querySelectorAll('.delete-btn');
+    });
+  }
 }
